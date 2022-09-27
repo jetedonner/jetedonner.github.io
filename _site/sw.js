d5e57bbb6e6119d4867ba6d1153b15a4a18b0dc8
Jetedonner <kim@kimhauser.ch>
2022-09-27 18:50:31 +0200

    PlayerStartPlugin cleanup etc

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220926.115154';
+const cacheName = 'chirpy-20220927.044303';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
