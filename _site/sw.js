cebffb13114af9e5cb5347a77a9a0fc23c172c1f
Jetedonner <kim@kimhauser.ch>
2022-12-31 12:46:42 +0100

    After merge

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221231.124323';
+const cacheName = 'chirpy-20221231.124634';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
